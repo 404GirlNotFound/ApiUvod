6a3588f19a03007c835946587a23d5cbcd22188f
404GirlNotFound <tia.podborsek@sts.sckr.si>
2025-09-17 07:29:06 +0000

    z for

--- app.py
+++ app.py
@@ -12,8 +12,9 @@ baseUrl = "https://www.google.com/"
 
 #API klic z JASON odgovorom
 baseUrl = "https://api.chucknorris.io/jokes/random"
-klic = requests.get(baseUrl)
-#preverimo .text, da je 100% JSON
-js = klic.json() #pretvorimo v dict
-#pprint.pprint(js)
-print(js.get("value"))
+for i in range(10):
+    klic = requests.get(baseUrl)
+    #preverimo .text, da je 100% JSON
+    js = klic.json() #pretvorimo v dict
+    #pprint.pprint(js)
+    print(js.get("value"))
